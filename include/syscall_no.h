2603aa30655edf261d063d99a2908eb28ed56146
Tony Zhao <tony.zx@hotmail.com>
2013-01-20 14:34:34 -0500

    syscall_no.h

--- include/syscall_no.h
+++ include/syscall_no.h
@@ -1,16 +1,9 @@
 #ifndef __SYSCALL_NO_H__
 #define __SYSCALL_NO_H__
-<<<<<<< HEAD
-
-=======
->>>>>>> 8e664c515ac195f387880710fbee281105600bc5
 
 #define SYS_exit		0
 #define SYS_create		1
 #define SYS_pass		2
 
-<<<<<<< HEAD
-#endif //__SYSCALL_NO_H__
-=======
+
 #endif // __SYSCALL_NO_H__
->>>>>>> 8e664c515ac195f387880710fbee281105600bc5
