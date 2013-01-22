8a52a353271cab667d7d7afc12b0ad810f9ee146
Greg Wang <gregwym@gmail.com>
2013-01-22 01:26:25 -0500

    Fix debug print macro; define dbflags in the same header

--- include/debug.h
+++ include/debug.h
@@ -1,13 +1,10 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
- 
-// Not using extern, define your own static dbflags in each file
-// extern unsigned int dbflags;
- 
+
 /*
  * Bit flags for DEBUG()
  */
-// #define DB_PLIO        0x001
+#define DB_SYSCALL     0x001
 // #define DB_TIMER       0x002
 // #define DB_USER_INPUT  0x004
 // #define DB_TRAIN_CTRL  0x008
@@ -19,11 +16,9 @@
 // #define DB_NET         0x200
 // #define DB_NETFS       0x400
 // #define DB_KMALLOC     0x800
- 
-#if 0
-#define DEBUG(d, fmt, ...) (((dbflags) & (d)) ? bwprintf(COM2, fmt, __VA_ARGS__) : 0)
-#else
+
+#define dbflags DB_SYSCALL
+
 #define DEBUG(d, fmt, args...) (((dbflags) & (d)) ? bwprintf(COM2, fmt, ##args) : 0)
-#endif
- 
+
 #endif // __DEBUG_H__
