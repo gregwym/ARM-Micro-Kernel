04cb9a96cf7fff84399b588682b35c3433dc515c
Greg Wang <gregwym@gmail.com>
2013-01-23 13:52:35 -0500

    Implement Create

--- syscall.c
+++ syscall.c
@@ -4,7 +4,27 @@
 #include <usertrap.h>
 
 int Create(int priority, void (*code) ()) {
+	int rtn = -1;
+	// If priority is invalid, return -1
+	if (priority < 0 || priority >= TASK_PRIORITY_MAX ) {
+		return rtn;
+	}
+
+	// Setup parameters and callno
+	void *parameters[3] = {(&priority), (&code), NULL};
+	asm("mov r0, #1");
+	asm("mov r1, %0"
+	    :
+	    :"r"(parameters));
+
+	// Enter kernel
 	asm("swi 1");
+
+	// Return the return value from kernel
+	asm("mov %0, r0"
+	    :"=r"(rtn)
+	    :);
+	return rtn;
 }
 
 int MyTid() {
