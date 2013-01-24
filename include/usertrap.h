f601f3c442fd7415c6c08fc45bae9a642890632a
Greg Wang <gregwym@gmail.com>
2013-01-23 21:44:01 -0500

    initTrap now return the sp after saved user tf

--- include/usertrap.h
+++ include/usertrap.h
@@ -1,5 +1,5 @@
 void kernelEntry();
 void kernelExit(void *resume_point);
 void switchCpuMode(int flag);
-void initTrap(void *sp, void *exit_syscall);
+void *initTrap(void *sp, void *exit_syscall);
 void activateStack(void *sp);
