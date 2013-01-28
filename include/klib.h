0dd84fb3fcd087a7b51f4fd1d06152120eb63c40
Greg Wang <gregwym@gmail.com>
2013-01-27 19:42:55 -0500

    Move KernelGlobal to klib.h

--- include/klib.h
+++ include/klib.h
@@ -4,6 +4,13 @@
 #ifndef __KLIB_H__
 #define __KLIB_H__
 
+typedef struct kernel_global {
+	TaskList 	*task_list;
+	FreeList 	*free_list;
+	BlockedList	*blocked_list;
+	MsgBuffer	*msg_array;
+	Task	 	*task_array;
+} KernelGlobal;
 
 void assert(int boolean, char * msg);
 
