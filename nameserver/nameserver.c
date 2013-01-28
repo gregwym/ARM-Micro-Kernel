5240e0149be5cceaafb7b7f59d37eb1289bb218e
Tony Zhao <tony.zx@hotmail.com>
2013-01-27 22:27:23 -0500

    Implement nameserver

---
+++ nameserver/nameserver.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <syscall_handler.h>
+#include <bwio.h>
+#include <usertrap.h>
+#include <syscall.h>
+
+#define NAME_SERVER_TID 1
+
+
+int RegisterAs( char *name ) {
+	int msglen = strlen(name);
+	char reply[6];
+	int retval = -1;
+	if (Send(NAME_SERVER_TID, name, msglen, reply, 6) >= 0) {
+		
+		// bwprintf(COM2, "reply msg size: %d\n", strlen(reply));
+		char *val = &retval;
+		val[0] = reply[0];
+		val[1] = reply[1];
+		val[2] = reply[2];
+		val[3] = reply[3];
+		if (retval == 1) {
+			return 0;
+		} else {
+			assert(retval != -1, "Forget R for register");
+			return -2;
+		}
+	} else {
+		return -1;
+	}
+}	
+
+int WhoIs( char *name ) {
+	int msglen = strlen(name);
+	char reply[5];
+	int retval = -1;
+	if (Send(NAME_SERVER_TID, name, msglen, reply, 5) >= 0) {
+		char *val = &retval;
+		val[0] = reply[0];
+		val[1] = reply[1];
+		val[2] = reply[2];
+		val[3] = reply[3];
+		if (retval == 0) {
+			return 0;
+		} else {
+			assert(retval != -1, "Forget W for whois");
+			return retval;
+		}
+	} else {
+		return -1;
+	}
+}
