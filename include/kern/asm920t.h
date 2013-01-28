7372da3f37c5e2ce478164b12b220103eb4abb7a
Greg Wang <gregwym@gmail.com>
2013-01-27 19:05:44 -0500

    Organized all include/kern/ headers

---
+++ include/kern/asm920t.h
@@ -0,0 +1,11 @@
+#ifndef _ARM920T_H_
+#define _ARM920T_H_
+
+#define SWI_ENTRY_POINT 0x28
+#define DATA_REGION_BASE 0x218000
+
+#define CPU_MODE_USER 0x10
+#define CPU_MODE_SVC 0x13
+#define CPU_MODE_SYS 0x1f
+
+#endif // _ARM920T_H_
---
+++ include/kern/callno.h
@@ -0,0 +1,13 @@
+#ifndef _KERN_CALLNO_H_
+#define _KERN_CALLNO_H_
+
+#define SYS_exit		0
+#define SYS_create		1
+#define SYS_pass		2
+#define SYS_myTid		3
+#define SYS_myParentTid	4
+#define SYS_send		5
+#define SYS_receive		6
+#define SYS_reply		7
+
+#endif // _KERN_CALLNO_H_
