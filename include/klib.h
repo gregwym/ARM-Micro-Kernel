6bb83977c4bbd881cece92480dc6540e89cb3dc0
Greg Wang <gregwym@gmail.com>
2013-01-28 00:59:03 -0500

    Change strlen, add copyBytes

--- include/klib.h
+++ include/klib.h
@@ -14,13 +14,14 @@ typedef struct kernel_global {
 	Task	 	*task_array;
 } KernelGlobal;
 
-void assert(int boolean, char * msg);
-
 /* String functions */
 int strcmp(const char *src, const char *dst);
 char *strcpy(char *dst, const char *src);
 char *strncpy(char *dst, const char *src, int n);
 int strlen(const char *s);
+
+/* Memory manipulations */
+void copyBytes(char *dst, const char *src);
 
 /* Print */
 #include <intern/bwio.h>
