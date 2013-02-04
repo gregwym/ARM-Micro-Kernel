c53013cc4bd3071e75c3161c16a9207a1c2611db
Tony Zhao <tony.zx@hotmail.com>
2013-02-03 20:03:42 -0500

    Fix: increase stack size, and fix clock-service. Add clock-server test.

--- services/clockserver.c
+++ services/clockserver.c
@@ -1,7 +1,9 @@
 #include <services.h>
 #include <klib.h>
 #include <unistd.h>
+#include <task.h>
 
+#define CS_TID						2
 #define CS_QUERY_TYPE_DELAY 		1
 #define CS_QUERY_TYPE_DELAY_UNTIL 	2
 #define CS_QUERY_TYPE_TIME			3
@@ -42,68 +44,42 @@ void notifier() {
 	}
 }
 
-void timeclient_p3() {
+int Delay( int ticks ) {
+	assert(ticks >= 0, "Delay ticks is negative");
+	char reply[1];
 	DelayQuery query;
 	query.type = CS_QUERY_TYPE_DELAY;
-	query.delay_tick = 10;
-	char msg[3] = "ts";;
-	char reply[1];
-	int time_server_tid = WhoIs(msg);
-	int counter = 0;
-	while (counter < 20) {
-		Send(time_server_tid, (char*)(&query), sizeof(DelayQuery), reply, 1);
-		counter++;
-	}
+	query.delay_tick = (unsigned int) ticks;
+	return Send(CS_TID, (char*)(&query), sizeof(DelayQuery), reply, 1);
 }
 
-void timeclient_p4() {
-	DelayQuery query;
-	query.type = CS_QUERY_TYPE_DELAY;
-	query.delay_tick = 23;
-	char msg[3] = "ts";;
+int DelayUntil( int ticks ) {
+	assert(ticks >= 0, "DelayUntil ticks is negative");
 	char reply[1];
-	int time_server_tid = WhoIs(msg);
-	int counter = 0;
-	while (counter < 9) {
-		Send(time_server_tid, (char*)(&query), sizeof(DelayQuery), reply, 1);
-		counter++;
-	}
+	DelayUntilQuery query;
+	query.type = CS_QUERY_TYPE_DELAY_UNTIL;
+	query.delay_time = (unsigned int) ticks;
+	return Send(CS_TID, (char*)(&query), sizeof(DelayUntilQuery), reply, 1);
 }
-
-void timeclient_p5() {
-	DelayQuery query;
-	query.type = CS_QUERY_TYPE_DELAY;
-	query.delay_tick = 33;
-	char msg[3] = "ts";;
-	char reply[1];
-	int time_server_tid = WhoIs(msg);
-	int counter = 0;
-	while (counter < 6) {
-		Send(time_server_tid, (char*)(&query), sizeof(DelayQuery), reply, 1);
-		counter++;
-	}
-}
-
-void timeclient_p6() {
-	DelayQuery query;
-	query.type = CS_QUERY_TYPE_DELAY;
-	query.delay_tick = 71;
-	char msg[3] = "ts";;
-	char reply[1];
-	int time_server_tid = WhoIs(msg);
-	int counter = 0;
-	while (counter < 3) {
-		Send(time_server_tid, (char*)(&query), sizeof(DelayQuery), reply, 1);
-		counter++;
-	}
+	
+int Time() {
+	int ret = -1;
+	TimeQuery send_query;
+	ReplyQuery reply_query;
+	send_query.type = CS_QUERY_TYPE_TIME;
+	ret = Send(CS_TID, (char*)(&send_query), sizeof(TimeQuery), (char*)(&reply_query), sizeof(ReplyQuery));
+	if (ret < 0) return ret;
+	return reply_query.time;
 }
 
 void clockserver() {
 	unsigned int time = 0;
-	char msg[3] = "ts";
+	char msg[3];
+	msg[0] = 't';
+	msg[1] = 's';
+	msg[2] = '\0';
 	ReplyQuery reply;
 	RegisterAs(msg);
-	int notifier_tid = Create(3, notifier);
 	ClockServerMsg message;
 	int tid;
 	
@@ -118,13 +94,15 @@ void clockserver() {
 	for (i = 0; i < TASK_MAX; i++) {
 		tid_array[i] = -1;
 	}
+	int notifier_tid = Create(1, notifier);
 	
 	while (1) {
 		Receive(&tid, (char *)&message, sizeof(ClockServerMsg));
 		if (tid == notifier_tid) {
 			Reply(tid, NULL, 0);
 			time++;
-			if (minheap.heapsize > 0 && time > *(int *)(minheap.data[0]->datum)) {
+			DEBUG(DB_CS, "| CS:\tTime : %d\n", time);
+			if (minheap.heapsize > 0 && time >= minheap.data[0]->key) {
 				HeapNode *top = minHeapPop(&minheap);
 				Reply(*(int *)(top->datum), NULL, 0);
 			}	
@@ -139,10 +117,14 @@ void clockserver() {
 					break;
 				case 2:
 					// delay until
-					tid_array[tid % TASK_MAX] = tid;
-					nodes[tid % TASK_MAX].key = message.delayQuery.delay_tick;
-					nodes[tid % TASK_MAX].datum = &(tid_array[tid % TASK_MAX]);
-					minHeapInsert(&minheap, &(nodes[tid % TASK_MAX]));
+					if (message.delayUntilQuery.delay_time <= time) {
+						Reply(tid, NULL, 0);
+					} else {
+						tid_array[tid % TASK_MAX] = tid;
+						nodes[tid % TASK_MAX].key = message.delayUntilQuery.delay_time;
+						nodes[tid % TASK_MAX].datum = &(tid_array[tid % TASK_MAX]);
+						minHeapInsert(&minheap, &(nodes[tid % TASK_MAX]));
+					}
 					break;
 				case 3:
 					// time
