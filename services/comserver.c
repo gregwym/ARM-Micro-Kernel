d7a4a9ffa8fbabcada0890d2303ed7259921a959
Greg Wang <gregwym@gmail.com>
2013-02-18 12:51:23 -0500

    Add comments, and use constant COM_NAME_ARRAY_LEN

--- services/comserver.c
+++ services/comserver.c
@@ -8,6 +8,7 @@
 
 #define COM1_REG_NAME 	"C1"
 #define COM2_REG_NAME 	"C2"
+#define COM_NAME_ARRAY_LEN	3
 
 typedef struct putc_query {
 	int type;
@@ -29,11 +30,11 @@ void com1SendNotifier() {
 	char c1_name[] = COM1_REG_NAME;
 	int com1_server_tid = WhoIs(c1_name);
 	assert(com1_server_tid >= 0, "Notifier cannot find com1 server's tid");
-	
+
 	char reply[2];
 	reply[1] = '\0';
 	char send[] = "";
-	
+
 	while (1) {
 		Send(com1_server_tid, send, 1, reply, 2);
 		AwaitEvent(EVENT_COM1_TX , reply, sizeof(char));
@@ -44,10 +45,10 @@ void com1ReceiveNotifier() {
 	char c1_name[] = COM1_REG_NAME;
 	int com1_server_tid = WhoIs(c1_name);
 	assert(com1_server_tid >= 0, "Notifier cannot find com1 server's tid");
-	
+
 	char send[2];
 	send[1] = '\0';
-	
+
 	while (1) {
 		AwaitEvent(EVENT_COM1_RX , send, sizeof(char));
 		Send(com1_server_tid, send, 2, NULL, 0);
@@ -58,11 +59,11 @@ void com2SendNotifier() {
 	char c2_name[] = COM2_REG_NAME;
 	int com2_server_tid = WhoIs(c2_name);
 	assert(com2_server_tid >= 0, "Notifier cannot find com2 server's tid");
-	
+
 	char reply[2];
 	reply[1] = '\0';
 	char send[] = "";
-	
+
 	while (1) {
 		Send(com2_server_tid, send, 1, reply, 2);
 		AwaitEvent(EVENT_COM2_TX , reply, sizeof(char));
@@ -73,10 +74,10 @@ void com2ReceiveNotifier() {
 	char c2_name[] = COM2_REG_NAME;
 	int com2_server_tid = WhoIs(c2_name);
 	assert(com2_server_tid >= 0, "Notifier cannot find com2 server's tid");
-	
+
 	char send[2];
 	send[1] = '\0';
-	
+
 	while (1) {
 		AwaitEvent(EVENT_COM2_RX , send, sizeof(char));
 		Send(com2_server_tid, send, 2, NULL, 0);
@@ -127,22 +128,22 @@ void comserver() {
 	int server_type = -1;
 	Receive(&tid, (char *)(&server_type), sizeof(int));
 	Reply(tid, NULL, 0);
-	
-	char c_name[3] = "";
-	if(server_type == COM1) strncpy(c_name, COM1_REG_NAME, 2);
-	else if(server_type == COM2) strncpy(c_name, COM2_REG_NAME, 2);
-	
+
+	char c_name[COM_NAME_ARRAY_LEN] = "";
+	if(server_type == COM1) strncpy(c_name, COM1_REG_NAME, COM_NAME_ARRAY_LEN);
+	else if(server_type == COM2) strncpy(c_name, COM2_REG_NAME, COM_NAME_ARRAY_LEN);
+
 	assert(strlen(c_name) != 0, "Invalid COM server name");
 	assert(RegisterAs(c_name) == 0, "COM server register failed");
-	
+
 	CharBuffer send_buffer;
 	CharBuffer receive_buffer;
-	
+
 	int send_notifier_tid;
 	int receive_notifier_tid;
 	char send_array[COM_BUFFER_SIZE];
 	char receive_array[COM_BUFFER_SIZE];
-	
+
 	cBufferInitial(&send_buffer, send_array, COM_BUFFER_SIZE);
 	cBufferInitial(&receive_buffer, receive_array, COM_BUFFER_SIZE);
 	if (server_type == COM1) {
@@ -152,39 +153,50 @@ void comserver() {
 		send_notifier_tid = Create(1, com2SendNotifier);
 		receive_notifier_tid = Create(1, com2ReceiveNotifier);
 	}
-	
+
 	IOMsg message;
 	char reply[2];
 	reply[1] = '\0';
-	
-	int ready_to_send_to_char_getter = 0;
-	int ready_to_send_to_com = 0;
+
+	int char_getter_is_waiting = 0;
+	int send_notifier_is_waiting = 0;
 	int char_getter_tid = 0;
-	
+
 	while(1) {
-		Receive(&tid, (char *)&message, sizeof(IOMsg));
+		/* Receive and process a message */
+		Receive(&tid, (char *)(&message), sizeof(IOMsg));
+
+		// If is from send notifier, set send_notifier_is_waiting
 		if (tid == send_notifier_tid) {
-			ready_to_send_to_com = 1;
-		} else if (tid == receive_notifier_tid) {
-			cBufferPush(&receive_buffer, message.ch);
-			Reply(tid, reply, 1);
-		} else {
-			if (message.type == IO_QUERY_TYPE_GETC) {
-				ready_to_send_to_char_getter = 1;
-				char_getter_tid = tid;
-			} else if (message.type == IO_QUERY_TYPE_PUTC) {
-				cBufferPush(&send_buffer, message.putcQuery.ch);
-				Reply(tid, reply, 1);
-			}
+			send_notifier_is_waiting = 1;
 		}
-		if (ready_to_send_to_com && send_buffer.current_size > 0) {
+		// Or is from receive notifier,
+		else if (tid == receive_notifier_tid) {
+			// Reply FIRST, then push the char to receive_buffer
+			Reply(tid, reply, 1);
+			cBufferPush(&receive_buffer, message.ch);
+		}
+		// Or is a getc msg, set char_getter_is_waiting and save its tid
+		else if (message.type == IO_QUERY_TYPE_GETC) {
+			char_getter_is_waiting = 1;
+			char_getter_tid = tid;
+		}
+		// Or is a putc msg,
+		else if (message.type == IO_QUERY_TYPE_PUTC) {
+			// Push the char to send_buffer, then reply
+			cBufferPush(&send_buffer, message.putcQuery.ch);
+			Reply(tid, reply, 1);
+		}
+
+		/* Serve waiting getter/send notifier */
+		if (send_notifier_is_waiting && send_buffer.current_size > 0) {
 			reply[0] = cBufferPop(&send_buffer);
-			ready_to_send_to_com = 0;
+			send_notifier_is_waiting = 0;
 			Reply(send_notifier_tid, reply, 2);
 		}
-		if (ready_to_send_to_char_getter && receive_buffer.current_size > 0) {
+		if (char_getter_is_waiting && receive_buffer.current_size > 0) {
 			reply[0] = cBufferPop(&receive_buffer);
-			ready_to_send_to_char_getter = 0;
+			char_getter_is_waiting = 0;
 			Reply(char_getter_tid, reply, 2);
 		}
 	}
