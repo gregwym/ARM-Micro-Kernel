cc779ba9ce58a6e1aafa116b45e047ecdb44b524
Greg Wang <gregwym@gmail.com>
2013-02-18 15:11:41 -0500

    Combine com1 & com2 notifiers

--- services/comserver.c
+++ services/comserver.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 
 #define COM_BUFFER_SIZE		100
+#define COM_NOTIFIER_PRIORITY	1
+
 #define IO_QUERY_TYPE_GETC	0
 #define IO_QUERY_TYPE_PUTC	1
 
@@ -26,61 +28,63 @@ typedef union {
 	GetcQuery	getcQuery;
 } IOMsg;
 
-void com1SendNotifier() {
-	char c1_name[] = COM1_REG_NAME;
-	int com1_server_tid = WhoIs(c1_name);
-	assert(com1_server_tid >= 0, "Notifier cannot find com1 server's tid");
+typedef struct io_reply {
+	int	value;
+} IOReply;
 
+typedef struct server_info {
+	int server_tid;
+	int channel_id;
+} ServerInfo;
+
+ServerInfo receiveChannelInfo() {
+	int server_tid = -1;
+	int channel_id = -1;
+	int result = Receive(&server_tid, (char *)(&channel_id), sizeof(int));
+	assert(result > 0, "Notifier receive channel info failed");
+	assert(channel_id == COM1 || channel_id == COM2, "Notifier received invalid channel id");
+	Reply(server_tid, NULL, 0);
+
+	ServerInfo serverInfo;
+	serverInfo.server_tid = server_tid;
+	serverInfo.channel_id = channel_id;
+
+	return serverInfo;
+}
+
+void comSendNotifier() {
+	ServerInfo serverInfo = receiveChannelInfo();
+	unsigned int eventId = (serverInfo.channel_id == COM1 ? EVENT_COM1_TX :
+															EVENT_COM2_TX);
 	char reply[2];
 	reply[1] = '\0';
-	char send[] = "";
+	char send = '\0';
+	int result = -1;
 
 	while (1) {
-		Send(com1_server_tid, send, 1, reply, 2);
-		AwaitEvent(EVENT_COM1_TX , reply, sizeof(char));
+		result = Send(serverInfo.server_tid, &send, sizeof(char), reply, sizeof(char));
+		assert(result == sizeof(char), "Send Notifier got invalid reply size");
+
+		result = AwaitEvent(eventId , reply, sizeof(char));
+		assert(result == 0, "Send Notifier wait event failed");
 	}
 }
 
-void com1ReceiveNotifier() {
-	char c1_name[] = COM1_REG_NAME;
-	int com1_server_tid = WhoIs(c1_name);
-	assert(com1_server_tid >= 0, "Notifier cannot find com1 server's tid");
+void comReceiveNotifier() {
+	ServerInfo serverInfo = receiveChannelInfo();
+	unsigned int eventId = (serverInfo.channel_id == COM1 ? EVENT_COM1_RX :
+															EVENT_COM2_RX);
 
 	char send[2];
 	send[1] = '\0';
+	int result = -1;
 
 	while (1) {
-		AwaitEvent(EVENT_COM1_RX , send, sizeof(char));
-		Send(com1_server_tid, send, 2, NULL, 0);
-	}
-}
+		result = AwaitEvent(eventId , send, sizeof(char));
+		assert(result == 0, "Receive Notifier wait event failed");
 
-void com2SendNotifier() {
-	char c2_name[] = COM2_REG_NAME;
-	int com2_server_tid = WhoIs(c2_name);
-	assert(com2_server_tid >= 0, "Notifier cannot find com2 server's tid");
-
-	char reply[2];
-	reply[1] = '\0';
-	char send[] = "";
-
-	while (1) {
-		Send(com2_server_tid, send, 1, reply, 2);
-		AwaitEvent(EVENT_COM2_TX , reply, sizeof(char));
-	}
-}
-
-void com2ReceiveNotifier() {
-	char c2_name[] = COM2_REG_NAME;
-	int com2_server_tid = WhoIs(c2_name);
-	assert(com2_server_tid >= 0, "Notifier cannot find com2 server's tid");
-
-	char send[2];
-	send[1] = '\0';
-
-	while (1) {
-		AwaitEvent(EVENT_COM2_RX , send, sizeof(char));
-		Send(com2_server_tid, send, 2, NULL, 0);
+		result = Send(serverInfo.server_tid, send, 2, NULL, 0);
+		assert(result == 0, "Receive Notifier sent to server failed");
 	}
 }
 
@@ -90,13 +94,13 @@ int Getc(int channel) {
 	getc_query.type = IO_QUERY_TYPE_GETC;
 	char reply[2];
 	if (channel == 1) {
-		char c_name[] = COM1_REG_NAME;
-		int com1_server_tid = WhoIs(c_name);
+		char server_name[] = COM1_REG_NAME;
+		int com1_server_tid = WhoIs(server_name);
 		assert(com1_server_tid > 0, "Getc failed to get com1server's tid");
 		ret = Send(com1_server_tid, (char *)&getc_query, sizeof(GetcQuery), reply, 2);
 	} else {
-		char c_name[] = COM2_REG_NAME;
-		int com2_server_tid = WhoIs(c_name);
+		char server_name[] = COM2_REG_NAME;
+		int com2_server_tid = WhoIs(server_name);
 		assert(com2_server_tid > 0, "Getc failed to get com1server's tid");
 		ret = Send(com2_server_tid, (char *)&getc_query, sizeof(GetcQuery), reply, 2);
 	}
@@ -110,13 +114,13 @@ int Putc(int channel, char ch) {
 	putc_query.type = IO_QUERY_TYPE_PUTC;
 	putc_query.ch = ch;
 	if (channel == 1) {
-		char c_name[] = COM1_REG_NAME;
-		int com1_server_tid = WhoIs(c_name);
+		char server_name[] = COM1_REG_NAME;
+		int com1_server_tid = WhoIs(server_name);
 		assert(com1_server_tid > 0, "Putc failed to get com1server's tid");
 		ret = Send(com1_server_tid, (char *)&putc_query, sizeof(PutcQuery), NULL, 0);
 	} else {
-		char c_name[] = COM2_REG_NAME;
-		int com2_server_tid = WhoIs(c_name);
+		char server_name[] = COM2_REG_NAME;
+		int com2_server_tid = WhoIs(server_name);
 		assert(com2_server_tid > 0, "Putc failed to get com1server's tid");
 		ret = Send(com2_server_tid, (char *)&putc_query, sizeof(PutcQuery), NULL, 0);
 	}
@@ -124,36 +128,39 @@ int Putc(int channel, char ch) {
 }
 
 void comserver() {
+	// Receive serving channel id
 	int tid;
-	int server_type = -1;
-	Receive(&tid, (char *)(&server_type), sizeof(int));
+	int channel_id = -1;
+	Receive(&tid, (char *)(&channel_id), sizeof(int));
 	Reply(tid, NULL, 0);
 
-	char c_name[COM_NAME_ARRAY_LEN] = "";
-	if(server_type == COM1) strncpy(c_name, COM1_REG_NAME, COM_NAME_ARRAY_LEN);
-	else if(server_type == COM2) strncpy(c_name, COM2_REG_NAME, COM_NAME_ARRAY_LEN);
+	// Define server_name and register
+	char server_name[COM_NAME_ARRAY_LEN] = "";
+	if(channel_id == COM1) strncpy(server_name, COM1_REG_NAME, COM_NAME_ARRAY_LEN);
+	else if(channel_id == COM2) strncpy(server_name, COM2_REG_NAME, COM_NAME_ARRAY_LEN);
+	assert(strlen(server_name) != 0, "Invalid COM server name");
+	assert(RegisterAs(server_name) == 0, "COM server register failed");
 
-	assert(strlen(c_name) != 0, "Invalid COM server name");
-	assert(RegisterAs(c_name) == 0, "COM server register failed");
-
+	// Create send and receive buffer
 	CharBuffer send_buffer;
 	CharBuffer receive_buffer;
-
-	int send_notifier_tid;
-	int receive_notifier_tid;
 	char send_array[COM_BUFFER_SIZE];
 	char receive_array[COM_BUFFER_SIZE];
 
 	cBufferInitial(&send_buffer, send_array, COM_BUFFER_SIZE);
 	cBufferInitial(&receive_buffer, receive_array, COM_BUFFER_SIZE);
-	if (server_type == COM1) {
-		send_notifier_tid = Create(1, com1SendNotifier);
-		receive_notifier_tid = Create(1, com1ReceiveNotifier);
-	} else {
-		send_notifier_tid = Create(1, com2SendNotifier);
-		receive_notifier_tid = Create(1, com2ReceiveNotifier);
-	}
 
+	// Create send and receive notifier
+	int send_notifier_tid;
+	int receive_notifier_tid;
+	send_notifier_tid = Create(COM_NOTIFIER_PRIORITY, comSendNotifier);
+	receive_notifier_tid = Create(COM_NOTIFIER_PRIORITY, comReceiveNotifier);
+
+	// Send server info to notifiers
+	Send(send_notifier_tid, (char *)(&channel_id), sizeof(int), NULL, 0);
+	Send(receive_notifier_tid, (char *)(&channel_id), sizeof(int), NULL, 0);
+
+	//
 	IOMsg message;
 	char reply[2];
 	reply[1] = '\0';
@@ -173,7 +180,7 @@ void comserver() {
 		// Or is from receive notifier,
 		else if (tid == receive_notifier_tid) {
 			// Reply FIRST, then push the char to receive_buffer
-			Reply(tid, reply, 1);
+			Reply(tid, NULL, 0);
 			cBufferPush(&receive_buffer, message.ch);
 		}
 		// Or is a getc msg, set char_getter_is_waiting and save its tid
@@ -192,7 +199,7 @@ void comserver() {
 		if (send_notifier_is_waiting && send_buffer.current_size > 0) {
 			reply[0] = cBufferPop(&send_buffer);
 			send_notifier_is_waiting = 0;
-			Reply(send_notifier_tid, reply, 2);
+			Reply(send_notifier_tid, reply, sizeof(char));
 		}
 		if (char_getter_is_waiting && receive_buffer.current_size > 0) {
 			reply[0] = cBufferPop(&receive_buffer);
