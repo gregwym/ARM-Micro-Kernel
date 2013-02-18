364bf0b8d9e8c8cb7236f4403a93d1f95505706c
Greg Wang <gregwym@gmail.com>
2013-02-17 22:47:01 -0500

    Add COM1/2 TX/RX events

--- include/kern/unistd.h
+++ include/kern/unistd.h
@@ -16,6 +16,10 @@
 
 /* System-wide Event ID */
 #define EVENT_TIME_ELAP	0	/* Time elapsed event */
-#define EVENT_MAX		1
+#define EVENT_COM1_TX	1
+#define EVENT_COM1_RX	2
+#define EVENT_COM2_TX	3
+#define EVENT_COM2_RX	4
+#define EVENT_MAX		5
 
 #endif // _KERN_UNISTD_H_
