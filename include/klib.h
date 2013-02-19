fb95dedd52981bd4cccf3ced709cc07892ced69d
Tony Zhao <tony.zx@hotmail.com>
2013-02-19 01:27:03 -0500

    change klib/h include issue

--- include/klib.h
+++ include/klib.h
@@ -66,7 +66,7 @@ void iBufferPush(IntBuffer *int_buffer, int i);
 int iBufferPop(IntBuffer *int_buffer);
 
 /* Print */
-#include <intern/stdio.h>
+#include <intern/irqio.h>
 #include <intern/bwio.h>
 #include <intern/debug.h>
 
