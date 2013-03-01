2c6e7829f682fac39002e38973818069e957a58f
Greg Wang <gregwym@gmail.com>
2013-03-01 15:34:01 -0500

    Handle reverse and switch command. Add switch table

--- include/train.h
+++ include/train.h
@@ -4,10 +4,14 @@
 /* Train Server */
 #include <intern/track_data.h>
 
+#define SYSTEM_START 96
+#define SYSTEM_STOP 97
+
 #define SWITCH_STR 33
 #define SWITCH_CUR 34
 #define SWITCH_OFF 32
 #define SWITCH_TOTAL 22
+#define SWITCH_INDEX_MOD 134
 #define SWITCH_NAMING_BASE 1
 #define SWITCH_NAMING_MAX 18
 #define SWITCH_NAMING_MID_BASE 153
@@ -18,8 +22,11 @@
 #define SENSOR_BYTE_SIZE 8
 #define SENSOR_BIT_MASK 0x1
 
+#define TRAIN_REVERSE 15
+
 typedef struct train_global {
 	track_node	*track_nodes;
+	char		*switch_table;
 } TrainGlobal;
 
 typedef enum train_msg_type {
--- train/train_center.c
+++ train/train_center.c
@@ -3,7 +3,47 @@
 #include <services.h>
 #include <train.h>
 
-void handleSensorUpdate(char *new_data, char *saved_data, char *buf) {
+#define DELAY_REVERSE 200
+#define DELAY_SWITCH 40
+
+/* Helpers */
+inline int switchIdToIndex(int id) {
+	return id % SWITCH_INDEX_MOD - 1;
+}
+
+/* Workers */
+void switchChanger(int switch_id, int direction) {
+	char cmd[2];
+	cmd[0] = direction;
+	cmd[1] = switch_id;
+	Puts(COM1, cmd, 2);
+
+	// Delay 400ms then turn off the solenoid
+	Delay(DELAY_SWITCH);
+
+	Putc(COM1, SWITCH_OFF);
+}
+
+void trainReverser(int train_id, int new_speed) {
+	char cmd[2];
+	cmd[0] = 0;
+	cmd[1] = train_id;
+	Puts(COM1, cmd, 2);
+
+	// Delay 2s then turn off the solenoid
+	Delay(DELAY_REVERSE);
+
+	// Reverse
+	cmd[0] = TRAIN_REVERSE;
+	Puts(COM1, cmd, 2);
+
+	// Re-accelerate
+	cmd[0] = new_speed;
+	Puts(COM1, cmd, 2);
+}
+
+/* Infomation Handlers */
+inline void handleSensorUpdate(char *new_data, char *saved_data, char *buf) {
 	int i, j;
 	char old_byte, new_byte, old_bit, new_bit, decoder_id;
 
@@ -38,18 +78,17 @@ void handleSensorUpdate(char *new_data, char *saved_data, char *buf) {
 	}
 }
 
-void handleSwitchCommand(int id, int value, char *switch_data, char *buf) {
+inline void handleSwitchCommand(int id, int value, char *switch_table, char *buf) {
 	sprintf(buf, "sw: %d -> %c\n", id, value);
 
 	value = value == 'C' ? SWITCH_CUR : SWITCH_STR;
+	CreateWithArgs(1, switchChanger, id, value, 0, 0);
 
-	// Normalize the id
-	id = id > SWITCH_NAMING_MAX ? value - SWITCH_NAMING_MID_BASE + SWITCH_NAMING_MAX + 1 : id;
-
-	switch_data[id] = value;
+	switch_table[switchIdToIndex(id)] = value;
 	Puts(COM2, buf, 0);
 }
 
+/* Train Center */
 void trainCenter() {
 	/* TrainGlobal */
 	TrainGlobal *train_global;
@@ -68,10 +107,11 @@ void trainCenter() {
 	}
 
 	/* SwitchData */
-	char switch_data[SWITCH_TOTAL];
+	char switch_table[SWITCH_TOTAL];
 	for(i = 0; i < SWITCH_TOTAL; i++) {
-		switch_data[i] = '?';
+		switch_table[i] = '?';
 	}
+	train_global->switch_table = switch_table;
 
 	cmd_tid = Create(7, trainCmdNotifier);
 	sensor_tid = Create(7, trainSensorNotifier);
@@ -83,9 +123,6 @@ void trainCenter() {
 		result = Receive(&tid, (char *)(&msg), sizeof(TrainMsg));
 		assert(result >= 0, "TrainCentral receive failed");
 
-		sprintf(str_buf, "Got msg, type: %d\n", msg.type);
-		Puts(COM2, str_buf, 0);
-
 		switch (msg.type) {
 			case SENSOR_DATA:
 				Reply(tid, NULL, 0);
@@ -93,21 +130,28 @@ void trainCenter() {
 				break;
 			case CMD_SPEED:
 				Reply(tid, NULL, 0);
+				// TODO: Send command to the train
 				str_buf[0] = msg.cmd_msg.value;
 				str_buf[1] = msg.cmd_msg.id;
 				Puts(COM1, str_buf, 2);
 				break;
+			case CMD_REVERSE:
+				Reply(tid, NULL, 0);
+				// TODO: Send command to the train
+				CreateWithArgs(1, trainReverser, msg.cmd_msg.id, 10, 0, 0);
+				break;
+			case CMD_SWITCH:
+				Reply(tid, NULL, 0);
+				handleSwitchCommand(msg.cmd_msg.id, msg.cmd_msg.value, switch_table, str_buf);
+				break;
 			case CMD_QUIT:
 				Halt();
 				break;
-			case CMD_REVERSE:
-			case CMD_SWITCH:
-				Reply(tid, NULL, 0);
-				handleSwitchCommand(msg.cmd_msg.id, msg.cmd_msg.value, switch_data, str_buf);
 			default:
+				sprintf(str_buf, "Got unknown msg, type: %d\n", msg.type);
+				Puts(COM2, str_buf, 0);
 				break;
 		}
-
 
 		str_buf[0] = '\0';
 	}
