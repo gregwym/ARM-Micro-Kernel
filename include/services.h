82d8d39cf7e2e41ac0204e17acf6a667e8f03399
Greg Wang <gregwym@gmail.com>
2013-03-01 22:05:45 -0500

    Rearrange header

--- include/services.h
+++ include/services.h
@@ -7,15 +7,15 @@ int RegisterAs( char *name );
 int WhoIs( char *name );
 
 /* Clock Server */
-#define COM1_REG_NAME 			"C1"
-#define COM2_REG_NAME 			"C2"
-
 void clockserver();
 int Delay( int ticks );
 int DelayUntil( int ticks );
 int Time( );
 
 /* IO Server */
+#define COM1_REG_NAME 			"C1"
+#define COM2_REG_NAME 			"C2"
+
 void comserver();
 int Getc(int channel);
 int Putc(int channel, char ch);
