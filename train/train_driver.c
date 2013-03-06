54f8779c7bdd45c36df866fd7629cf5e9acab8af
Greg Wang <gregwym@gmail.com>
2013-03-05 22:19:08 -0500

    Implement Dijkstra (untested)

--- train/train_driver.c
+++ train/train_driver.c
@@ -59,6 +59,91 @@ int calcDistance(track_node *src, track_node *dest, int depth, int distance) {
 	return -1;
 }
 
+inline void dijkstra_update(Heap *dist_heap, HeapNode *heap_nodes, track_node **previous,
+                            track_node *u, track_node *neighbour, int alter_dist) {
+	// Find the old distance
+	HeapNode *heap_node = &(heap_nodes[neighbour->index]);
+	int old_dist = heap_nodes[neighbour->index].key;
+
+	// If it is a node not in the heap, or has a shorter alter distance
+	if(old_dist == -1 || alter_dist < old_dist) {
+		// Assign the new distance and previous
+		heap_node->key = alter_dist;
+		previous[neighbour->index] = u;
+		if(old_dist == -1) minHeapInsert(dist_heap, heap_node);
+		else minHeapResortNode(dist_heap, heap_node);
+	}
+}
+
+int dijkstra(const track_node *track_nodes, const track_node *src,
+              const track_node *dest, track_node **route) {
+	int i;
+	track_node *previous[TRACK_MAX];
+	memset(previous, (int) NULL, sizeof(track_node *) * TRACK_MAX);
+
+	Heap dist_heap;
+	HeapNode *heap_data[TRACK_MAX];
+	HeapNode heap_nodes[TRACK_MAX];
+
+	// Initialize the heap with -1 dist and landmark as datum
+	heapInitial(&dist_heap, heap_data, TRACK_MAX);
+	for(i = 0; i < TRACK_MAX; i++) {
+		heap_nodes[i].key =  -1;
+		heap_nodes[i].datum = (void *)&(track_nodes[i]);
+	}
+
+	// Source's distance is 0, and insert into the min heap
+	heap_nodes[src->index].key = 0;
+	minHeapInsert(&dist_heap, &(heap_nodes[src->index]));
+
+	HeapNode *u_node = NULL;
+	int u_dist = -1;
+	track_node *u = NULL;
+	track_node *neighbour = NULL;
+	int alter_dist = -1;
+
+	while(dist_heap.heapsize > 0) {
+		// Pop the smallest distance vertex. Since node with infinit (-1) dist
+		// has not been inserted into the heap, no need to check distance
+		u_node = minHeapPop(&dist_heap);
+		u_dist = u_node->key;
+		u = u_node->datum;
+
+		// If find destination
+		if(u == dest) break;
+
+		switch(u->type) {
+			case NODE_ENTER:
+			case NODE_SENSOR:
+			case NODE_MERGE:
+				alter_dist = u_dist + u->edge[DIR_AHEAD].dist;
+				neighbour = u->edge[DIR_AHEAD].dest;
+				dijkstra_update(&dist_heap, heap_nodes, previous, u, neighbour, alter_dist);
+				break;
+			case NODE_BRANCH:
+				alter_dist = u_dist + u->edge[DIR_STRAIGHT].dist;
+				neighbour = u->edge[DIR_STRAIGHT].dest;
+				dijkstra_update(&dist_heap, heap_nodes, previous, u, neighbour, alter_dist);
+
+				alter_dist = u_dist + u->edge[DIR_CURVED].dist;
+				neighbour = u->edge[DIR_CURVED].dest;
+				dijkstra_update(&dist_heap, heap_nodes, previous, u, neighbour, alter_dist);
+				break;
+			default:
+				break;
+		}
+	}
+
+	assert(u == dest, "Dijkstra failed to find the destination");
+
+	for(i = TRACK_MAX - 1; previous[u->index] != NULL; i--) {
+		route[i] = u;
+		u = previous[u->index];
+	}
+
+	return i;
+}
+
 void trainDriver(TrainGlobal *train_global, TrainData *train_data) {
 	track_node *track_nodes = train_global->track_nodes;
 	int tid, result;
@@ -70,6 +155,7 @@ void trainDriver(TrainGlobal *train_global, TrainData *train_data) {
 
 	TrainMsg msg;
 	char str_buf[1024];
+	char *buf_cursor = str_buf;
 
 	// Initialize trian speed
 	setTrainSpeed(train_id, speed, com1_tid);
@@ -77,6 +163,9 @@ void trainDriver(TrainGlobal *train_global, TrainData *train_data) {
 	track_node *prev_landmark = &(track_nodes[0]);
 	track_node *cur_landmark = &(track_nodes[0]);
 	int dist_traveled = -1;
+
+	track_node *route[TRACK_MAX];
+	int route_start = TRACK_MAX;
 
 	while(1) {
 		result = Receive(&tid, (char *)(&msg), sizeof(TrainMsg));
@@ -106,6 +195,13 @@ void trainDriver(TrainGlobal *train_global, TrainData *train_data) {
 				Reply(tid, NULL, 0);
 				sprintf(str_buf, "T#%d routing to %s\n", train_id, track_nodes[msg.location_msg.value].name);
 				Puts(com2_tid, str_buf, 0);
+
+				route_start = dijkstra(track_nodes, cur_landmark, &(track_nodes[msg.location_msg.value]), route);
+				for(; route_start < TRACK_MAX; route_start++) {
+					buf_cursor += sprintf(buf_cursor, "%s -> ", route[route_start]->name);
+				}
+				buf_cursor += sprintf(buf_cursor, "\n");
+				Puts(com2_tid, str_buf, 0);
 				break;
 			default:
 				sprintf(str_buf, "Driver got unknown msg, type: %d\n", msg.type);
@@ -114,6 +210,7 @@ void trainDriver(TrainGlobal *train_global, TrainData *train_data) {
 		}
 
 		str_buf[0] = '\0';
+		buf_cursor = str_buf;
 	}
 }
 
